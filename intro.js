d149e7ae70b6a7c24e796fb6421a25952b3a1a8b
shishirgupta <shishirgupta071998@gmail.com>
2024-01-29 18:26:39 +0530

    l1

--- intro.js
+++ intro.js
@@ -1,3 +1,42 @@
-console.log("Shishir Gupta");
+//console.log("Shishir Gupta");
 
 //declare variable
+            // let a; //JS tells us it is a variable and it type is in users hand whatever user want, they can assign and by default value is undefined
+            // a=26;
+            // console.log(a);
+
+// variable type -> premitive types : string, number, boolean, null;
+
+        // loop and conditions are almost same as java
+            // let number=10;
+            // for(let i=1;i<=number;i++)
+            // {
+            //     console.log(i);
+            // }
+
+
+// non premitives : arrays, objects and function
+
+
+//function
+        // function sayHi()
+        // {
+        //     console.log("helllooo");
+
+        // }
+        // sayHi();
+
+// function with parameter
+        // function sayHi(param)
+        // {
+        //     console.log(param);
+        //     return "we can return anything according to requirment -> number, boolean, string, null";    
+        // }
+        // sayHi(10);//number
+        // sayHi("shishir");//string
+        // sayHi([10,34,345,45]);//array
+
+// if we are not passing anything than by default it will be undefined 
+        // console.log(sayHi());  // we are printing return value;
+
+
